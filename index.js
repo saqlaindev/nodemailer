355fc1c3418eee5502f2fa588d0aac4cfa86d4f5
saqlain-codes <saqlain@vectorwide.io>
2022-01-06 12:16:33 +0500

    chng

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const indexRouter = require('./routes/main');
 const rootRouter = require('./routes/root');
 app.use("/user", indexRouter);
-app.use("/", rootRouter);
+app.use("/display", rootRouter);
 const port =process.env.PORT|| 3000;
 app.listen(port,()=>{
     console.log(`server is  running on port ${port}`)
