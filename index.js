c744f58252bcb77b45e884eff7cf6580616dc82f
saqlain-codes <saqlain@vectorwide.io>
2022-01-06 12:11:35 +0500

    changes

--- index.js
+++ index.js
@@ -1,19 +1,12 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-const server=http.createServer(app)  // create server
 
 const indexRouter = require('./routes/main');
 
-
 app.use("/user", indexRouter);
 app.use("/", indexRouter);
-
-
-
-
-
-const port =process.env.PORT|| 5000;
-server.listen(port,()=>{
+const port =process.env.PORT|| 3000;
+app.listen(port,()=>{
     console.log(`server is  running on port ${port}`)
 })
